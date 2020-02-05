69b4b6f3a6b5d6b208bfbbeccd3264589edc7fd3
Leandro de Oliveira <leandro-smi@hotmail.com>
2020-02-05 16:41:23 -0300

    Update cotacao.py

--- cotacao.py
+++ cotacao.py
@@ -4,18 +4,14 @@ Autor: Fábio Berbert de Paula <prefeito@fabio.city>
 Data : 26/11/2018
 """
 
-#pip install requests-html
-from requests_html import HTMLSession
+import bs4 as bs
 
-def cotacao():
-    session = HTMLSession()
+import urllib.request
 
-    #URL resultado da busca no Google por: cotação dólar
-    r = session.get('https://www.google.com.br/search?q=cota%C3%A7%C3%A3o+dolar&oq=cota%C3%A7%C3%A3o+dolar&aqs=chrome..69i64j0l5.2721j1j4&sourceid=chrome&ie=UTF-8')
+fonte = urllib.request.urlopen('https://dolarhoje.com/').read()
 
-    #ID do span HTML que exibe o valor da cotação no resultado de busca
-    selector = '#knowledge-currency__tgt-amount'
+soup = bs.BeautifulSoup(fonte, 'lxml') #pega so o html
 
-    return "R$ " + r.html.find(selector, first=True).text
+dolar = soup.find("input", {"id": "nacional"})
 
-print(cotacao())
+print(dolar.get('value'))
